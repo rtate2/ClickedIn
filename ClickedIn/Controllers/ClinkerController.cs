d6c52a98c4820781086c782749ce2937ec06f76a
Randy Tate <randytatejr@gmail.com>
2020-04-06 20:58:45 -0500

    planned and set code template

---
+++ ClickedIn/Controllers/ClinkerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClickedIn.Controllers
+{
+    [Route("api/clinker")]
+    [ApiController]
+    public class ClinkerController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult AddClinker(Clinker clinkerToAdd)
+        {
+            return NotFound("In progress");
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetClinker(int Id)
+        {
+            return NotFound("In progress");
+        }
+
+        [HttpGet("{Interest}")]
+        public IActionResult GetClinkerByInterest(string Interest)
+        {
+            return NotFound("In progress");
+        }
+    }
+}
---
+++ ClickedIn/Models/ClinkerProfile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClickedIn.Models
+{
+    public class ClinkerProfile
+    {
+        public int Id { get; set; }
+        public Clinker Clinker { get; set; }
+        public List<Clinker> Homies { get; set; }
+        public List<Clinker> Enemies { get; set; }
+    }
+}
